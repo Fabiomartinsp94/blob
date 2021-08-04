ab21390127e861976d69eadfa6cb689a3e229da5
Guilherme Campiani <guilherme.campiani@gmail.com>
2021-08-04 12:28:55 -0300

    Update [id].js

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -6,10 +6,17 @@ import { getAllPostIds, getPostData } from '../../lib/posts'
 import Head from "next/head";
 import Photo from "../../components/round_photo/rount_photo";
 
+import unified from 'unified';
+import parse from 'remark-parse';
+import remark2react from 'remark-react';
 
 
 
 export default function Post({ postData }) {
+  const content = unified()
+    .use(parse)
+    .use(remark2react)
+    .processSync(postData.contentHtml).result;
   
   return (
     <Layout>
@@ -17,11 +24,13 @@ export default function Post({ postData }) {
         <title>{postData.title}</title>
       </Head>
       <article>
+      <div className={utilStyles.liner}>
         <h1 className={utilStyles.title}>{postData.title}</h1>
           <Date dateString={postData.date} />
-        <div className={utilStyles.liner}>
         </div>
-        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+        <div>
+            {content}
+        </div>
       </article>
       <section>
       <Photo names={[postData.author]}
