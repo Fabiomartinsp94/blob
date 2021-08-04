9eedc57333e8dd6ecd4f55b5a0620bc07b0532ca
Guilherme Campiani <guilherme.campiani@gmail.com>
2021-08-04 11:43:45 -0300

    Update index.js

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -14,7 +14,7 @@ export default function Posts({ allPostsData }) {
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title, author, description }) => (
-            <Link className={utilStyles.withLink} href={`/posts/${id}`}>
+            <Link className={utilStyles.withLink} href={`/posts/${encodeURIComponent(id)}`}>
               <Post date={date} title={title} author={author} description={description} key={id}></Post>
             </Link>
           ))}
